de4537c7143bf7cb3b0cbc956dbbbde288a04e3a
Natalia-Duzhak <72571464+Natalia-Duzhak@users.noreply.github.com>
2020-12-13 23:48:10 +0300

    Add files via upload

---
+++ UnitTest9.2А/UnitTest9.2А.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project9.2А/Source.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest92А
+{
+	TEST_CLASS(UnitTest92А)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int N = 0;
+			int m = 0;
+			 Sort(0, N);
+			Assert::AreEqual(m, 0);
+		}
+	};
+}
